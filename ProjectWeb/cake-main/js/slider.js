d6b39bf990b11ed145118f4cd66da06f59dbb4fb
bjruly <bumjruly06@gmail.com>
2025-10-23 20:16:40 +0700

    xong users

--- ProjectWeb/cake-main/js/slider.js
+++ ProjectWeb/cake-main/js/slider.js
@@ -1,16 +1,10 @@
 isLoggedIn = false;
 function checkLogin(event) {
   if (!isLoggedIn) {
-    event.preventDefault(); // chặn chuyển trang
-
-    // Bước 1: cảnh báo ban đầu
+    event.preventDefault();
     alert("⚠️ Bạn phải đăng nhập trước khi xem chi tiết!");
-
-    // Bước 2: hỏi người dùng có muốn đăng nhập không
     const confirmLogin = confirm("Bạn có muốn đến trang đăng nhập không?");
-
     if (confirmLogin) {
-      // Nếu người dùng bấm OK → chuyển sang trang đăng nhập
       window.location.href = "login.html";
     }
   }
